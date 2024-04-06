e7fa2916541f57b116b6c6f70b53df2eaedd7150
Тоторо <roman.aka.tod@gmail.com>
2024-04-06 13:52:30 +0300

    Done

---
+++ main.py
@@ -0,0 +1,33 @@
+from http.server import BaseHTTPRequestHandler, HTTPServer
+
+hostName = "localhost"
+serverPort = 8080
+
+
+class MyServer(BaseHTTPRequestHandler):
+
+    @staticmethod
+    def __get_index():
+        with open('index.html', 'r', encoding='utf-8') as content:
+            result = content.read()
+        return result
+
+    def do_GET(self):
+        page_content = self.__get_index()
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes(page_content, "utf-8"))
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server started http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
